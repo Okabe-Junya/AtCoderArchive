1b1de54f9f55f23561dd450c66530210df56cc6f
Okabe-Junya <okabe.junya.qj@alumni.tsukuba.ac.jp>
2022-02-04 16:24:05 +0900

    061

---
+++ Other/Typical90/061.py
@@ -0,0 +1,13 @@
+from collections import deque
+
+Q = int(input())
+d = deque()
+
+for _ in range(Q):
+    t, x = map(int, input().split())
+    if t == 1:
+        d.appendleft(x)
+    elif t == 2:
+        d.append(x)
+    else:
+        print(d[x - 1])
