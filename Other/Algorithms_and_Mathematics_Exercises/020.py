b9f766c43d70bb6836f2d32eb7aa4152fcd21aeb
Okabe-Junya <okabe.junya.qj@alumni.tsukuba.ac.jp>
2022-02-09 16:40:20 +0900

    020

--- Other/Algorithms_and_Mathematics_Exercises/020.py
+++ Other/Algorithms_and_Mathematics_Exercises/020.py
@@ -1,7 +1,13 @@
 n = int(input())
-f(n)
+a = list(map(int, input().split()))
 
-def f(n):
-    if n == 1:
-        print(n)
-    return n * f(n - 1)
+ans = 0
+for i in range(n):
+    for j in range(i + 1, n):
+        for k in range(j + 1, n):
+            for l in range(k + 1, n):
+                for m in range(l + 1, n):
+                    tmp = a[i] + a[j] + a[k] + a[l] + a[m]
+                    if tmp == 1000:
+                        ans += 1
+print(ans)
