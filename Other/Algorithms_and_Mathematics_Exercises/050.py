afda10991358894fd7b602226b2b77cd62e35434
Okabe-Junya <okabe.junya.qj@alumni.tsukuba.ac.jp>
2022-02-11 01:47:03 +0900

    050

---
+++ Other/Algorithms_and_Mathematics_Exercises/050.py
@@ -0,0 +1,13 @@
+# xのn乗をmで割った余り
+def pos(x, n, m):
+    if n == 0:
+        return 1
+    res = pos((x ** 2) % m, n // 2, m)
+    if n % 2 == 1:
+        res = res * x % m
+    return res
+
+
+a, b = map(int, input().split())
+INF = 10 ** 9 + 7
+print(pos(a, b, INF) % INF)
