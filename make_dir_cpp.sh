c7f49c5d0722f6037d2acd019f38aacf28d8105c
Okabe-Junya <okabe.junya.qj@alumni.tsukuba.ac.jp>
2022-02-15 03:32:19 +0900

    cpp

---
+++ make_dir_cpp.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+if [ $# -ne 3 ]
+then
+    echo "Usage: $0 <dir_name> <start> <end>"
+    exit 1
+fi
+
+cd $1
+start=$2
+end=$3
+if [ $start -gt $end ]
+then
+    echo "<start> must be less than <end>"
+    exit 1
+fi
+while [ $start -le $end ]
+do
+    # 0埋め
+    tmp="000${start}"
+    dir_name="${tmp: -3}"
+
+    mkdir $dir_name
+    cd $dir_name
+    for var in a b c d e f
+    do
+        filename="${var}.cpp"
+        touch $filename
+    done
+    cd ..
+    start=`expr $start \+ 1`
+done
