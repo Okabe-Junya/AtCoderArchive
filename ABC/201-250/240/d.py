0177210c674218e95ed1676dca16474675aa1e31
Okabe-Junya <okabe.junya.qj@alumni.tsukuba.ac.jp>
2022-02-23 01:02:54 +0900

    d (re)

--- ABC/201-250/240/d.py
+++ ABC/201-250/240/d.py
@@ -1,26 +1,14 @@
 n = int(input())
 a = list(map(int, input().split()))
-cnt = 0
-tmp = 0
-ans = 0
-cnt_sum = 0
+stack = [[0, 0]]
 for i in range(n):
-    ans += 1
-    if a[i] == tmp:
-        cnt += 1
-        if tmp == cnt:
-            cnt_sum += cnt
-            ans -= cnt
-            if i - cnt_sum >= 0:
-                tmp = a[i - cnt_sum]
-            else:
-                tmp = 0
-            #print("tmp:", tmp)
-            cnt = 1
-            while (i - cnt_sum - cnt >= 0) and (a[i - cnt_sum - cnt] == tmp):
-                cnt += 1
-            #print("cnt:", cnt)
+    if a[i] != stack[-1][0]:
+        stack.append([a[i], 1])
     else:
-        cnt = 1
-        tmp = a[i]
-    print(ans)
+        stack.append([a[i], stack[-1][1] + 1])
+    
+    if stack[-1][0] == stack[-1][1]:
+        for _ in range(stack[-1][0]):
+            stack.pop()
+    #print(stack)
+    print(len(stack) - 1)
