ea64f083e00ac5580c208011a6656d0b7e5d126b
Okabe-Junya <okabe.junya.qj@alumni.tsukuba.ac.jp>
2022-03-08 18:57:28 +0900

    043(bfs)

--- Other/Algorithms_and_Mathematics_Exercises/043.py
+++ Other/Algorithms_and_Mathematics_Exercises/043.py
@@ -1,7 +1,5 @@
-import sys
+from collections import deque
 
-
-sys.setrecursionlimit(100000)
 n, m = map(int, input().split())
 edge = [[] for _ in range(n)]
 for _ in range(m):
@@ -10,19 +8,38 @@ for _ in range(m):
     edge[b - 1].append(a - 1)
 # print(edge)
 
-can = [False] * n
-
-
-def dfs(i):
-    can[i] = True
-    for i in edge[i]:
-        if not can[i]:
-            dfs(i)
-
-
-dfs(0)
-# print(can)
-if sum(can) == n:
+if n == 1:
     print("The graph is connected.")
+    exit()
+
+can = [-1] * n
+queue = deque([0])
+
+def bfs(G, d, queue):
+    """bfs: breadth-first search
+    Args:
+        G (list): G[i] is the list of neighbors of node i.
+        d (list): d[i] is the distance from the source to node i.
+        queue (collections.deque): queue of nodes to visit next.
+    Returns:
+        list: d[i] for all nodes i in the breadth-first order.
+            (if node i has no outgoing edges, then d[i] will be -1.)
+    """
+    
+    while queue:
+        next = queue.popleft()
+        for node in G[next]:
+            if d[node] == -1:
+                d[node] = d[next] + 1
+                queue.append(node)
+    return d
+
+can = bfs(edge, can, queue)
+
+# print(can)
+for i in can:
+    if i == -1:
+        print("The graph is not connected.")
+        break
 else:
-    print("The graph is not connected.")
+    print("The graph is connected.")
