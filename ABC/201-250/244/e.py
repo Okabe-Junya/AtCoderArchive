3821763979ebadae0b753b2f341c26ec643f06ee
Okabe-Junya <okabe.junya.qj@alumni.tsukuba.ac.jp>
2022-03-25 23:11:11 +0900

    re

--- ABC/201-250/244/e.py
+++ ABC/201-250/244/e.py
@@ -1,5 +1,3 @@
-from collections import deque
-
 INF = 998244353
 
 N, M, K, S, T, X = map(int, input().split())
@@ -8,29 +6,22 @@ for _ in range(M):
     a, b = map(int, input().split())
     G[a - 1].append(b - 1)
     G[b - 1].append(a - 1)
-
-queue = deque([S - 1])
-d = [-1] * N
-d[S - 1] = 0
-
-def bfs(G, d, queue):
-    """bfs: breadth-first search
-    Args:
-        G (list): G[i] is the list of neighbors of node i.
-        d (list): d[i] is the distance from the source to node i.
-        queue (collections.deque): queue of nodes to visit next.
-    Returns:
-        list: d[i] for all nodes i in the breadth-first order.
-            (if node i has no outgoing edges, then d[i] will be -1.)
-    """
     
-    while queue:
-        next = queue.popleft()
-        for node in G[next]:
-            if d[node] == -1:
-                d[node] = d[next] + 1
-                queue.append(node)
-    return 
 
-bfs(G, d, queue)
-print(d)
+dp = [[[0, 0] for _ in range(K + 1)] for _ in range(N)] 
+# dp[[i][j][0]] : Aj = i かつXを偶数回踏んでいる場合のかず
+# dp[[i][j][1]] : Aj = i かつXを奇数回踏んでいる場合のかず
+
+dp[S - 1][0][0] = 1
+for i in range(1, K + 1):
+    for j in range(N):
+        for k in G[j]:
+            if k != X - 1:
+                dp[k][i][0] += dp[j][i - 1][0]
+                dp[k][i][1] += dp[j][i - 1][1]
+            else:
+                dp[k][i][1] += dp[j][i - 1][0]
+                dp[k][i][0] += dp[j][i - 1][1]
+            dp[k][i][0] %= INF
+            dp[k][i][1] %= INF
+print(dp[T - 1][K][0] % INF)
