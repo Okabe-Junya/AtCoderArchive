ba6d2e5cbc35f689bef355a6c8755ca01fdf7434
Junya Okabe <okabe.junya.qj@alumni.tsukuba.ac.jp>
2022-11-02 02:26:22 +0900

    ぞい

--- ABC/251-300/275/c.py
+++ ABC/251-300/275/c.py
@@ -1,5 +1,3 @@
-
-
 S = []
 for _ in range(9):
     S.append(list(input()))
@@ -8,34 +6,17 @@ ans = 0
 
 
 def check(p1, p2, p3, p4):
-    # 与えられた4点が正方形か否か判定する．
-    # 4点が正方形ならTrueを返す．
-    if p1[0] == p2[0]:
-        # p1とp2が同じx座標を持つ
-        if p3[0] == p4[0]:
-            # p3とp4も同じx座標を持つ
-            if p1[1] == p3[1]:
-                # p1とp3が同じy座標を持つ
-                if p2[1] == p4[1]:
-                    # p2とp4も同じy座標を持つ
-                    if p1[1] - p2[1] == p3[1] - p4[1]:
-                        # p1とp2のx座標の差とp3とp4のy座標の差が等しい
-                        return True
-                    else:
-                        return False
-    else:
-        v1 = (p2[0] - p1[0], p2[1] - p1[1])
-        v2 = (p3[0] - p1[0], p3[1] - p1[1])
-        if v1[0] ** 2 + v1[1] ** 2 == v2[0] ** 2 + v2[1] ** 2:
-            # p1, p2, p3が直角三角形
-            v3 = (p4[0] - p1[0], p4[1] - p1[1])
-            if v1[0] * v3[0] + v1[1] * v3[1] == 0:
-                # p1, p2, p4が直角三角形
+    L = []
+    from itertools import combinations as cmb
+
+    for l1, l2 in cmb((p1, p2, p3, p4), 2):
+        L.append((l1[0] - l2[0]) ** 2 + (l1[1] - l2[1]) ** 2)
+        L.sort()
+    if L[0] == L[1] == L[2] == L[3]:
+        if L[-1] == L[-2]:
+            if L[-1] == 2 * L[0]:
                 return True
     return False
-
-
-
 
 
 for i in range(81 - 3):
@@ -53,7 +34,6 @@ for i in range(81 - 3):
                     and S[p4[0]][p4[1]] == "#"
                 ):
                     if check(p1, p2, p3, p4):
-                        print(p1, p2, p3, p4)
                         ans += 1
 
 print(ans)
